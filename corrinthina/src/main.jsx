a2dbb005137e38fb64800942f12deb5a9fce37e9
KugleBlitz007 <136974540+KugleBlitz007@users.noreply.github.com>
2025-04-18 22:31:19 -0400

    Deployed the website on gh pages
    Steps:
    - define the base of vite config as the repo name
    - introduce hash router in main
    - npm install react-router-dom
    - not forgetin tailwind
    - homepage in package.json
    - npm install gh-pages --save-dev
    - predeploy and deploy in package.json using dist
    - TODO: still figuring out why on custom domain it works

--- corrinthina/src/main.jsx
+++ corrinthina/src/main.jsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  HashRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
--- corrinthina/vite.config.js
+++ corrinthina/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Corrinthina_Portfolio/',
   plugins: [
     react(),
     tailwindcss()
